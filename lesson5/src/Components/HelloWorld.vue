0e02438de7d46cfdf3f4c85c2a8e6277f0fa4400
shaoqi <549730265@qq.com>
2017-06-23 17:46:58 +0800

    单个文件.vue
    Signed-off-by: shaoqi <549730265@qq.com>

---
+++ lesson5/src/Components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+    {{greeting}} world!
+    </p>
+</template>
+
+<script>
+    module.export={
+        data:function (){
+            return{
+                greeting:'Hello'
+            }
+        }
+    }
+</script>
